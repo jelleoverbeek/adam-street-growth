675b14a93c7e7ff1ed4518be16a1a01ab6b0e757
Jelle Overbeek <j.overbeek@recognize.nl>
2018-03-06 15:45:25 +0100

    added mapbox

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,6 +58,18 @@ const content = {
     }
 };
 
+const map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13
+});
+
+L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: config.mapbox_key
+}).addTo(map);
+
 const app = {
     init: function () {
         api.setData();
